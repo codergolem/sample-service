4df42fc7fc1f57b9ebbd9887ee87dab6cab48655
circleci-deployment <mario@bevy.com>
2022-04-03 13:40:00 -0500

    test final

--- run.sh
+++ run.sh
@@ -9,25 +9,28 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export REGION="us-east-1"
+
 export ACCOUNT_ID="651625262782"
+export REGION="us-east-1"
+CLUSTER_NAME="ecosia-app-development"
+export VERSION=${CIRCLE_SHA1}
 REGISTRY_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 SERVICE_NAME="sample"
-export VERSION=${CIRCLE_SHA1}
 FULL_IMAGE_NAME="${REGISTRY_URL}/${SERVICE_NAME}:${VERSION}"
 
 
 task_update() {
-    # aws ecr get-login-password \
-    #                                                 --region ${REGION} \
-    #                                                 | docker login \
-    #                                                 --username AWS \
-    #                                                 --password-stdin "${REGISTRY_URL}"
+    aws ecr get-login-password \
+                                                    --region ${REGION} \
+                                                    | docker login \
+                                                    --username AWS \
+                                                    --password-stdin "${REGISTRY_URL}"
     
-    # docker build . -t "${FULL_IMAGE_NAME}"
-    # docker push "${FULL_IMAGE_NAME}"
+    docker build . -t "${FULL_IMAGE_NAME}"
+    docker push "${FULL_IMAGE_NAME}"
     envsubst < "k8s/deployment.tpl" > "k8s/config/deployment.yml"
-    # kubectl apply -f service.yml
+    aws eks --region "${REGION}" update-kubeconfig --name "${CLUSTER_NAME}"
+    kubectl apply -f k8s/config
 }
 
 task_infra() {
